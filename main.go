8fc7a9eefbaf5bc2feb5f8b11a96db55e13d8eab
Tanay PrabhuDesai <tanayseven@gmail.com>
2024-08-12 19:55:50 +0530

    Add template rendering

--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	_ "github.com/GoAdminGroup/go-admin/adapter/echo" // Import the adapter, it must be imported. If it is not imported, you need to define it yourself.
 	"github.com/GoAdminGroup/go-admin/engine"
 	"github.com/GoAdminGroup/go-admin/modules/config"
 	_ "github.com/GoAdminGroup/go-admin/modules/db/drivers/sqlite" // Import the sql driver
+	"github.com/GoAdminGroup/go-admin/modules/language"
 	"github.com/GoAdminGroup/go-admin/tests/tables"
 	_ "github.com/GoAdminGroup/themes/adminlte" // Import the theme
-
-	"github.com/GoAdminGroup/go-admin/modules/language"
+	"html/template"
+	"io"
 
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
+
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+type Message struct {
+	Message string `json:"message"`
+}
 
 func main() {
 	cfg := config.Config{
@@ -37,10 +51,20 @@ func main() {
 	eng.AddConfig(&cfg).
 		AddGenerators(tables.Generators)
 
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("templates/*.gohtml")),
+	}
+
+	// Set the renderer
+	e.Renderer = renderer
+
 	_ = eng.Use(e)
 
+	newMap := make(map[string]interface{})
+	data, _ := json.Marshal(Message{Message: "Hello, World!"})
+	_ = json.Unmarshal(data, &newMap)
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.Render(http.StatusOK, "base.gohtml", newMap)
 	})
 
 	e.Logger.Fatal(e.Start(":8081"))
